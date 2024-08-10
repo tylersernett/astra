e37b67e269f0e84f1792a0812f07d438b0f7c94e
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2024-08-10 01:07:44 -0700

    Add specialities

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,35 @@
 
-<h1>about</h1>
-<p>this is the about page.</p>
+<h1 class='h1'>about</h1>
+<h2 class='h2'>Board Certified Accident Doctors</h2>
+<p>
+  We Are A Network Of Independent Medical Service Providers Working Together To Provide Care To Patients Injured From Car Accidents And Other Personal Injuries Covered Under A Medical Lien Or Personal Injury Protection
+</p>
+
+<h3 class='h3'>Riley M. Johnson, DO</h3>
+<p>
+  bio text
+</p>
+
+<hr/>
+<h2 class='h2'>Specialities</h2>
+<ul class="list-disc space-y-2 columns-1 md:columns-2">
+  <li>Pain Medicine</li>
+  <li>Sports Medicine</li>
+  <li>Orthopedic Surgery</li>
+  <li>Spine Surgery</li>
+  <li>General Surgery</li>
+  <li>ENT/Facial Surgery</li>
+  <li>Plastic and Reconstructive Surgery</li>
+  <li>PM&R Neurodiagnostics</li>
+  <li>Doctor of Dental Surgery (DDS)</li>
+  <li>Pharmacy</li>
+  <li>Physical Therapy</li>
+  <li>DBC Spine Rehabilitation</li>
+  <li>Accredited Pain Procedure Center</li>
+  <li>Accredited Imaging Centers (MRI, MR Arthrogram, CT, CT Arthrogram, X-ray, Ultrasound)</li>
+  <li>Accredited Hospital and Outpatient Surgical Center</li>
+  <li>Neuropsychology</li>
+  <li>Neuro-Optometry</li>
+  <li>Traumatic Brain Injury Rehabilitation</li>
+  <li>Work Conditioning and Return To Work Program</li>
+</ul>
