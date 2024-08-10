801f2f94db8bfc438f0ff035f6f5061e53afec9d
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2024-08-10 00:23:01 -0700

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,21 @@
 	<a href="/about">about</a>
   <a href="/FAQ">FAQ</a>
   <a href="/contact">contact</a>
-</nav>
-
-<slot></slot>
+  <svelte:fragment slot="pageFooter">
+    <div class="container mx-auto p-4 space-y-8">
+      <div class="flex justify-center items-center space-x-2">
+        <span>Astra Injury Rehabilitation Specialists</span>
+        <span>|</span>
+        <span>209 Saint Louis Ave, Fort Worth, TX 76104</span>
+        <span>|</span>
+        <span>817-897-5190</span>
+        <span>|</span>
+        <span>kdeosarran@astra-consultants.com</span>
+      </div>
+      <div class="text-center">
+        <br />
+        Copyright © 2024 Astra Injury Rehabilitation Specialists
+      </div>
+    </div>
+    
+  </svelte:fragment>
