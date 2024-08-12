4ac63d0a3231580308c3ed16d859638350ebac03
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2024-08-12 01:43:49 -0700

    Improve flex image display

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,63 +1,85 @@
-
-<h1 class='h1'>About</h1>
-<h2 class='h2'>Board Certified Accident Doctors</h2>
+<div class='text-center space-y-8 w-4/5 mx-auto max-w-[800px]'>
+<h1 class="h1">About</h1>
+<h2 class="h2">Board Certified Accident Doctors</h2>
 <p>
-  We Are A Network Of Independent Medical Service Providers Working Together To Provide Care To Patients Injured From Car Accidents And Other Personal Injuries Covered Under A Medical Lien Or Personal Injury Protection
+  We are a network of independent medical service providers working together to
+provide care to patients injured from car accidents and other personal
+injuries covered under a medical lien or personal injury protection.
 </p>
+</div>
 
-<h3 class='h3'>Riley M. Johnson, DO</h3>
+
 <!-- <Box display={'flex'} gap={'1em'} className='contact-flex'> -->
-<div class='flex'>
-<!-- style={{ flex: '1 1 50%', }} -->
-  <div class='flex-grow flex-shrink basis-5/6' >
-    <div id='physician-text' class='pr-2 space-y-4'>
-      <!-- style={{ textAlign: 'left' }} -->
-      <p>
-        Riley Johnson, DO, is a board certified, fellowship-trained interventional pain management physician.
-      </p>
-      <p>
-        A Texas native, he attended Churchill High School in San Antonio. He earned an undergraduate degree in Civil Engineering at the University of Texas at Austin. He went on to study medicine at the University of North Texas Health Science Center in Fort Worth, where he earned a Master's Degree and his Doctorate.
-      </p>
-      <p>
-        He completed his Physical Medicine and Rehabilitation residency training at Baylor University Medical Center in Dallas, Texas, where he was named outstanding resident of the year in his second year of training. Dr. Johnson also served as chief resident at Baylor during his fourth year of residency.
-      </p>
-      <p>
-        He went on to complete a fellowship in Interventional Pain Management at Virginia Commonwealth University in Richmond, Virginia.
-      </p>
-      <p>
-        Dr. Johnson serves as medical director at the Parkinson's <a href='https://www.balancetherapytoday.com/mindandbody/' class='anchor'>Mind Body Institute (MIND)</a>, a non-profit serving patients with Parkinson's Disease.
-      </p>
-      <p>
-        Dr. Johnson is a Diplomate of the <a href='https://www.abpmr.org/' class='anchor'>American Board of Physical Medicine & Rehabilitation</a>.
-      </p>
-      <p>
-        Dr. Johnson has interests in neuromodulation and regenerative medicine. He enjoys spending time with his wife and daughter, weight lifting, playing blues guitar, and enjoying live music.
-      </p>
-    </div>
-  </div>
-
+<div class="">
   <!-- style={{ flex: '1 1 50%', }} -->
-  <div class ='flex-grow flex-shrink basis-1/2' >
-    <img
-      srcSet="/riley-portrait-480w.png 480w, /riley-portrait-600w.png 600w, /riley-portrait-736w.png 736w,  /riley-portrait.png 911w"
-      sizes="(min-width: 1700px) 501px, (min-width: 1300px) 407px, (min-width: 1020px) 345px, (min-width: 860px) 33.57vw, (min-width: 740px) calc(50vw - 103px), (min-width: 500px) calc(100vw - 176px), (min-width: 360px) calc(100vw - 59px), calc(50vw + 111px)"
-      src="/riley-portrait.png"
-      alt="A portrait of Dr. Johnson smiling while wearing a navy blue suit in front of the Ft. Worth skyline"
-      class='w-full'
+  <div class="flex space-x-4">
+    <!-- style={{ flex: '1 1 50%', }} -->
+    <div id="bio-images" class="flex-col basis-5/6">
+      <img
+        srcSet="images/riley-portrait-480w.png 480w, images/riley-portrait-600w.png 600w, images/riley-portrait-736w.png 736w,  images/riley-portrait.png 911w"
+        sizes="(min-width: 1700px) 501px, (min-width: 1300px) 407px, (min-width: 1020px) 345px, (min-width: 860px) 33.57vw, (min-width: 740px) calc(50vw - 103px), (min-width: 500px) calc(100vw - 176px), (min-width: 360px) calc(100vw - 59px), calc(50vw + 111px)"
+        src="images/riley-portrait.png"
+        alt="A portrait of Dr. Johnson smiling while wearing a navy blue suit in front of the Ft. Worth skyline"
+        class="w-full rounded-full"
       />
       <!-- style={{ width: '100%', borderRadius: '6px' }} -->
-    <img
-      src="/abpmr-rust-830w.png"
-      alt="A circular logo for the American Board of Physical Medicine and Rehabilitation. The center contains a Rod of Asclepius flanked by two olive branches."
-      class='w-full my-4'
-    />
-    <!--       style={{ width: '100%', marginTop: '1em', marginBottom: '1em'  }} -->
+      <!-- <img
+        src="/abpmr-rust-830w.png"
+        alt="A circular logo for the American Board of Physical Medicine and Rehabilitation. The center contains a Rod of Asclepius flanked by two olive branches."
+        class="w-full my-4"
+      /> -->
+      <!--       style={{ width: '100%', marginTop: '1em', marginBottom: '1em'  }} -->
+    </div>
+    <div id="bio-text" class="space-y-4">
+    <!-- <div id="bio-text" class="pr-2 space-y-4"> -->
+      <!-- style={{ textAlign: 'left' }} -->
+      <h3 class="h3 font-bold">Riley M. Johnson, DO</h3>
+      <p>
+        Riley Johnson, DO, is a board certified, fellowship-trained
+        interventional pain management physician.
+      </p>
+      <p>
+        A Texas native, he attended Churchill High School in San Antonio. He
+        earned an undergraduate degree in Civil Engineering at the University of
+        Texas at Austin. He went on to study medicine at the University of North
+        Texas Health Science Center in Fort Worth, where he earned a Master's
+        Degree and his Doctorate.
+      </p>
+      <p>
+        He completed his Physical Medicine and Rehabilitation residency training
+        at Baylor University Medical Center in Dallas, Texas, where he was named
+        outstanding resident of the year in his second year of training. Dr.
+        Johnson also served as chief resident at Baylor during his fourth year
+        of residency.
+      </p>
+      <p>
+        He went on to complete a fellowship in Interventional Pain Management at
+        Virginia Commonwealth University in Richmond, Virginia.
+      </p>
+      <p>
+        Dr. Johnson serves as medical director at the Parkinson's <a
+          href="https://www.balancetherapytoday.com/mindandbody/"
+          class="anchor">Mind Body Institute (MIND)</a
+        >, a non-profit serving patients with Parkinson's Disease.
+      </p>
+      <p>
+        Dr. Johnson is a Diplomate of the <a
+          href="https://www.abpmr.org/"
+          class="anchor">American Board of Physical Medicine & Rehabilitation</a
+        >.
+      </p>
+      <p>
+        Dr. Johnson has interests in neuromodulation and regenerative medicine.
+        He enjoys spending time with his wife and daughter, weight lifting,
+        playing blues guitar, and enjoying live music.
+      </p>
+    </div>
   </div>
 </div>
 <!-- </Box> -->
 
-<hr/>
-<h2 class='h2'>Specialities</h2>
+<hr />
+<h2 class="h2">Specialities</h2>
 <ul class="list-disc space-y-2 columns-1 md:columns-2">
   <li>Pain Medicine</li>
   <li>Sports Medicine</li>
@@ -72,7 +94,10 @@
   <li>Physical Therapy</li>
   <li>DBC Spine Rehabilitation</li>
   <li>Accredited Pain Procedure Center</li>
-  <li>Accredited Imaging Centers (MRI, MR Arthrogram, CT, CT Arthrogram, X-ray, Ultrasound)</li>
+  <li>
+    Accredited Imaging Centers (MRI, MR Arthrogram, CT, CT Arthrogram, X-ray,
+    Ultrasound)
+  </li>
   <li>Accredited Hospital and Outpatient Surgical Center</li>
   <li>Neuropsychology</li>
   <li>Neuro-Optometry</li>
