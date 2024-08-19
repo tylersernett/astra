e2f136814875df00621f767ed195385c59d1b43c
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2024-08-18 17:32:51 -0700

    Add shadow, separate CTA

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,45 @@
-<div id='80%-width' class="mx-auto w-4/5 card p-4">
-  <h2 class="h2 text-center mb-8">
-    Board Certified Physicians Treating Injuries from Car Accidents in the Fort
-    Worth Area
-  </h2>
-  <div class=" space-y-4 mx-auto">
+<div class="max-w-[990px] p-8 mx-auto">
+  <div id="80%-width" class=" card shadow-xl p-4 md:p-8 my-8">
+    <h2 class="h2 text-center mb-8">
+      Board Certified Physicians Treating Injuries from Car Accidents in the
+      Fort Worth Area
+    </h2>
+    <div class=" space-y-4 mx-auto">
+      <p>
+        We are a network of board-certified physicians and surgeons (MD and DO),
+        doctors of dental surgery (DDS), imaging centers, pharmacies, surgery
+        centers, and hospitals providing medical care to personal injury
+        patients in Fort Worth, Texas and the surrounding areas. Physician
+        injury consultants doctors provide care to patients injured in car
+        accidents through a medical lien or personal injury protection in the
+        Fort Worth area.
+      </p>
+      <p>
+        It is important your injuries are diagnosed and treated as quickly as
+        possible, even if you do not have an attorney. Call us first to see a
+        doctor and begin treatment.
+      </p>
+      <p>
+        If you have been treated in an emergency room due to a car accident and
+        cannot find a doctor to see you for follow-up care, we can probably
+        help. We can arrange an appointment with a board-certified physician
+        experienced in treating injuries from motor vehicle accidents and
+        quickly obtain your ER treatment records for your doctor.
+      </p>
+    </div>
+  </div>
+  <div id="call-to-action" class="p-4 text-2xl text-center">
     <p>
-      We are a network of board-certified physicians and surgeons (MD and DO),
-      doctors of dental surgery (DDS), imaging centers, pharmacies, surgery
-      centers, and hospitals providing medical care to personal injury patients
-      in Fort Worth, Texas and the surrounding areas. Physician injury
-      consultants doctors provide care to patients injured in car accidents
-      through a medical lien or personal injury protection in the Fort Worth
-      area.
-    </p>
-    <p>
-      It is important your injuries are diagnosed and treated as quickly as
-      possible, even if you do not have an attorney. Call us first to see a
-      doctor and begin treatment.
-    </p>
-    <p>
-      If you have been treated in an emergency room due to a car accident and
-      cannot find a doctor to see you for follow-up care, we can probably help.
-      We can arrange an appointment with a board-certified physician experienced
-      in treating injuries from motor vehicle accidents and quickly obtain your
-      ER treatment records for your doctor.
-    </p>
-    <p>
-      Call our appointment line at <a href="tel:817-897-5190" class="anchor"
-        >817-897-5190</a
-      >
+      Call our appointment line at
+      <a href="tel:817-897-5190" class="anchor font-bold"> 817-897-5190 </a>
       or
-      <a class="anchor" href="mailto:kdeosarran@astra-consultants.com"
-        >email us</a
-      > today.
+      <a
+        class="anchor font-bold"
+        href="mailto:kdeosarran@astra-consultants.com"
+      >
+        email us
+      </a>
+      today.
     </p>
   </div>
 </div>
